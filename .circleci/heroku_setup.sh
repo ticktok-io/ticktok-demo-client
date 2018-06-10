ba629e6ff14295821d427b582b6b8c3ec0daf2ce
rockem <eli.segal@gmail.com>
2018-06-10 22:42:06 +0300

    Updated Heroku deployment

--- .circleci/heroku_setup.sh
+++ .circleci/heroku_setup.sh
@@ -6,6 +6,7 @@ wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+heroku plugins:install heroku-container-registry
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
